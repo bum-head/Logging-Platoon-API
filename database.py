4e7425a9b8cb6010a51b327f90729150761d505a
bum-head <isnotrealluck@gmail.com>
2025-08-07 00:32:45 +0530

    added some changes to server and the api as i had to make 3 tables in sqlgit add . :(

--- database.py
+++ database.py
@@ -17,7 +17,8 @@ class Database:
         self.db_conn = sql.connect(
                                     host = self.host,
                                     user = self.user,
-                                    password = self.passwd
+                                    password = self.passwd,
+                                    database = self.db
                                     )
 
         if self.db_conn == None:
@@ -27,10 +28,20 @@ class Database:
             return True  #ConnECted!@!
             
 
-    def add_post(self):
-        pass
+    def add_post(self, values):
+        if self.db_conn != None:
+            self.db_cursor.execute(f"INSERT INTO POSTS VALUES{values}")
+            self.db_conn.commit()
+            data = self.db_cursor.fetchall()
+            
+            if data == []: 
+                return True 
+        else:
+            return False
 
-    def get_all_post(self) --> dict:
+       
+
+    def get_all_post(self):
         if self.db_conn != None:
             self.db_cursor.execute("SELECT * FROM POSTS")
             data_columns = self.db_cursor.fetchall()
@@ -49,17 +60,54 @@ class Database:
             
             self.db_cursor.execute("DESCRIBE POSTS")
             data_headers = [desc[0] for desc in self.db_cursor.fetchall()]
+            data_headers = data_headers.append("tags")
+            
+            self.db_cursor.execute(f"SELECT * FROM post_tag WHERE post_id={post_id}")
+            data_tag_id = [tag_id[1] for tag_id in self.db_cursor.fetchall()]
+            
+            tag_list = []
+            for tag in range(len(data_tag_id)):
+                self.db_cursor.execute(f"SELECT * FROM TAGS WHERE id={tag}")
+                tag_list = tag_list.append(name[1] for name in self.db_cursor.fetchall())
+
+            data_columns[0] = data_columns[0] + tuple([tag_list])
 
             return [dict(zip(data_headers,i)) for i in range(len(data_columns))]
         else:
             return False
 
 
-    def get_keyword_post(self):
-        pass
+    def get_keyword_post(self, word):
+        if self.db_conn != None:
+            self.db_cursor.execute(f"SELECT * FROM POSTS WHERE title LIKE '%{word}%' OR content LIKE '%{word}%' OR category LIKE '%{word}%'")
+            data_columns = self.db_cursor.fetchall()
+        
+            self.db_cursor.execute("DESCRIBE POSTS")
+            data_headers = [desc[0] for desc in self.db_cursor.fetchall()]
 
-    def update_post(self):
-        pass
+            return [dict(zip(data_headers,i)) for i in range(len(data_columns))]
+        else:
+            return False
+
+
+    def update_post(self, post_id, changing_field, updated_field):
+        if type(updated_field) == str:
+            cmd = f"UPDATE POSTS SET {changing_field} = '{updated_field}' WHERE id={post_id}"
+        else: 
+            cmd = f"UPDATE POSTS SET {changing_field} = {updated_field} WHERE id={post_id}"
+        if self.db_conn != None:
+            self.db_cursor.execute(cmd)
+            self.db_conn.commit()
+            data_columns = self.db_cursor.fetchall()
+            if len(data_columns) == 0:  ##checks if the post exists or not
+                return 2                ##returns 2 as a err handling thingy
+            else:
+                self.db_cursor.execute(f"DELETE FROM POSTS WHERE id={post_id}")
+                self.db_conn.commit()   #commits the deletion
+
+        else:
+            return False
+
 
     def delete_post(self, post_id):
          if self.db_conn != None:
--- ploggingAPI.py
+++ ploggingAPI.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python
 
 from flask import Flask, request, jsonify
+from database import *
+from datetime import *
 import mysql.connector as sql
-
+import json
+import flask
 
 app = Flask(__name__)
 
+Database = Database("localhost", "root", "Root34##", "Altera")
+
+Database.connect_to_database()
 
 @app.get("/posts")
 def get_all_posts():
+    postlist = Database.get_all_post()
+    if not postlist: 
+        flask.abort(404, "No postlist Found!!")
     return jsonify(postlist), 200
 
-@app.get("/posts/<int:post_id>") ## post_id is substitute for the id?               
+@app.get("/posts/<int:post_id>") 
 def get_post():
-    return 200
+    postlist = Database.get_post(post_id)
+    return jsonify(postlist), 200
 
 @app.get("/posts?term=<string:word>")
 def get_post_filter():
+    word =  request.args.get('term')
+    #search the database with the `word` and display result
     return 
 
 @app.put("/posts/<int:post_id>")
@@ -29,7 +41,16 @@ def delete_post():
 
 @app.post("/posts")
 def add_post():
-    return 201    
+    data = json.loads(request.data) 
+    date_now = datetime.now()
+    date_now = date_now.strftime("%b %d %Y %H:%M:%S")
+
+    id = len(Database.get_all_post())+1
+
+    values = (id , data["title"], data["content"], data["category"], data["tags"], date_now, date_now)
+    result = Database.add_post(values)
+    if result == []:
+        return 201
 
 
 if __name__ == "__main__":
